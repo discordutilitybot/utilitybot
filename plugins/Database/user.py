3962723372c37cc261654da957639a649e70b23e
FFlop <andrewnijmeh1@gmail.com>
2020-08-20 12:22:24 -0400

    Finsised up the querys and functions for the user file (commands_sent post the data columns etc

--- plugins/Database/user.py
+++ plugins/Database/user.py
@@ -10,16 +10,26 @@ class User:
         self.bot = bot
         self.id = id
         self.messages = messages
+        self.commands_sent = commands_sent
         self.joined_at = joined_at
         # This means everytime it joins the database not the server's.
     
-    # Make a function to return the user id and the userid by a string
 
+async def post(self):
+    """""""
+    query = "SELECT * from users WHERE ID = $1"
+    assure_exclusive = await self.bot.db.fetch(query, self.id)
+    if len(assure_exclusive) == 0:
+        query = "INSERT INTO users ( id, commands_sent, joined_at, messages_sent ) VALUES ( $1, $2, $3, $4 )"
+        await bot.db.execute(query, self.id, self.commands_sent, self.messages_sent, self.joined_at)
 
     # A function to check the amount of commands a user has used
 
     @classmethod
     async def on_command(cls, bot, user: Union[Member, Discord_User]):
-        await bot.db.get_user(user.id)
+        await bot.db.get_user(user.id) # Make sure a user object is in db
+
+        query = "UPDATE users SET commands_sent = commands_sent +1 WHERE id = $1"
+        await bot.db.execute(query, user.id)
 
 
