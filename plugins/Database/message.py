544339ffc5483f8c0ab546a4724e1dc73ee5f528
FFlop <andrewnijmeh1@gmail.com>
2020-08-20 21:01:41 -0400

    Finished message object file so it updates the message row in the user table to add +1 message everytime a message is sent

--- plugins/Database/message.py
+++ plugins/Database/message.py
@@ -11,7 +11,7 @@ class Message(object):
         self.author_id = author_id
 
 
-
+    """Query to insert data into the messages table with its rows etc."""
     async def post(self):
         query = """INSERT INTO Messages ( message_id, guild_id, author_id, channel_id )
                 VALUES ($1 $2 $3 $4)
@@ -19,4 +19,14 @@ class Message(object):
 
         await bot.db.execute(query, self.message_id, self.guild_id, self.author_id, self.channel_id)
 
-        
+    """Would have posted the real message content/object but currently i do not have the money to afford more database space.."""
+    
+    @classmethod
+    async def on_message(cls, bot, message: Discord_Message) -> None:
+        user = await bot.db.get_user(message.author.id)  # assure a user gets a row
+        self = cls(bot=bot, message_id=message.id, guild_id=message.guild.id, channel_id=message.channel.id, author_id=message.author.id)
+
+        """Use the post function above with the query to insert into the messages table.."""
+        await self.post()
+        """Update the amount of messages for a user in the message row for the user table"""
+        await bot.db.execute('UPDATE users SET messages_sent = messages_sent + 1 WHERE id = $1', user.id)
