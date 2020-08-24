7feed125dd57363e3db7a48cf9f2fbe20ec1be0c
FFlop <andrewnijmeh1@gmail.com>
2020-08-24 09:57:32 -0400

    Modified config.example.py file

--- config.example.py
+++ config.example.py
@@ -4,4 +4,12 @@ TOKEN = # Bot token
 
 POSTGRES = # Postgres uri
 
+OAUTH = # Oauth token for discord login oauth for the website
+
+CLIENT_ID = # Application id for oauth..
+
+OAUTHREDIRECT = # Oauth redirect uri for user object
+
+
+
 
