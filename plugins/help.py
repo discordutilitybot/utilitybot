a1004ecf595fecbe62a8474f77bca6016b31b584
FFlop <andrewnijmeh1@gmail.com>
2020-08-27 12:18:20 -0400

    initial

--- plugins/help.py
+++ plugins/help.py
@@ -20,8 +20,10 @@ class Help(commands.Cog):
         embed.add_field(name=":tada: Fun Commands" value="coinflip, multiply, add, divide, subtract, blackjack, guess, say, roast, slap, punch, kick,", inline=False)
         embed.add_field(name=":frame_photo: Images", value="meme, cat, dog, slap, smile", inline=False) # Probably will add more pictures considering i find a api for it :/ 
         embed.add_field(name=":wrench:, Utility", value="poll, suggest, botlist, tag", inline=False)
-        embed.add_field(name=":information_source :Info", value="avatar, help, invite, serverinfo, userinfo")
-
+        embed.add_field(name=":information_source:Info", value="avatar, help, invite, serverinfo, userinfo")
+        embed.add_field(name=":hammer_pick Mod" value="kick, ban, mute, purge, unmute, unban," inline=False)
+	
+  
 
 def setup(bot):
     bot.add_cog(bot(Help))
