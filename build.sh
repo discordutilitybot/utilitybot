9efc6dd1be7cda7d20a92dabd35b8f092f4bd178
andrew <andrewnijmeh1@gmail.com>
2020-09-07 14:20:15 -0400

    Added more statements to check perms in the bot etc also fixed error pulling from bot direcotry rather than /utilitybot

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git pull %% cd bot/ %% docker build -t utilitybot .
+git pull %% cd utilitybot/ %% docker build -t utilitybot .
--- events/command_error.py
+++ events/command_error.py
@@ -31,14 +31,16 @@ class Commanderror(commands.Cog):
             await ctx.send("You cannot use commands in a private message please go to a server to run commands.")
         
         if isinstance(error, commands.MissingPermissions):
-            for roles in ctx.guild.roles:
-                if roles == Falses:
+            """Check if the user has the right permissions if they dont return the error if they do then return true"""
+            for roles in ctx.guild.permissions:
+                if roles == False:
                     await ctx.send("You dont have the permissions to perform this command")
                 else:
                     return True
             
         if isinstance(error, commands.BotMissingPermissions):
-            await ctx.send("I am missing permissions to perform this command :(.")
+           for roles in ctx.bot.permissions:
+               
 
         if isinstance(error, commands.NoPrivateMessage):
             await ctx.send("You need to run this command in a server for it to work")
@@ -47,13 +49,14 @@ class Commanderror(commands.Cog):
             td = datetime.timedelta(seconds=error.retry_after)
             await ctx.send(f"This command is on cooldown please wait {td.format_timespan(td)}", delete_after=5)
 
-        """Probably wont need this since the commands wont be NSFW."""
+        """Probably wont need this since the commands wont be NSFW. yuck."""
         if isinstance(error, commands.NSFWChannelRequired):
-            await ctx.send("You need to use this command in a NSFW channel.")
+                await ctx.send("You need to have a NSFW channel to use this command")
+            
         
         
 
-    
+
 """Add the cog"""
 def setup(bot):
     bot.add_cog(Commanderror(bot))
