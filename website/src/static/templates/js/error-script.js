44abbc9fa48cfb3770a0d3037ea0e3048c358d09
northernlights03 <65303441+northernlights03@users.noreply.github.com>
2020-09-12 15:32:49 -0400

    Update error-script.js

--- website/src/static/templates/js/error-script.js
+++ website/src/static/templates/js/error-script.js
@@ -1,3 +1,14 @@
+function randomColour() {
+    let box = document.getElementById('mainBox');
+    let colorTemplate = ['#afa09e','#8f847d','#a4a48a','#7d8f78','#83928f','#738da2','#8a8395','#959093'];
+
+    function randomColourPick(colours) {
+        return colorTemplate[Math.floor(Math.random()*colorTemplate.length)];
+     }
+
+     box.style.backgroundColor = randomColourPick();
+}
+
 function textPlace() {
 let textToBePlaced = document.getElementById('errorText');
 let words = ['The URL may be mispelled or not in the database. Try returning to the homepage.', 
@@ -15,3 +26,4 @@ function randomitem(words) {
 textToBePlaced.innerHTML = randomitem(words);
 }
 textPlace();
+randomColour();
