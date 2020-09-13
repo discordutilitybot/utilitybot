1516ba73b9fa12cf0cacc4cfc443bc5686535a50
northernlights03 <65303441+northernlights03@users.noreply.github.com>
2020-09-13 16:15:36 -0400

    Overlay navbar and basic design updates
    Added overlay navbar for tablets and phones. Footer updated

--- website/src/static/templates/js/nav.js
+++ website/src/static/templates/js/nav.js
@@ -1,11 +1,22 @@
 let nav = document.getElementById("navCollapsed");
 let openBtn = document.getElementById('openButton')
-function openNav() {
+
+function navView() {
+  if (nav.style.opacity = "0") {
     nav.style.opacity = "1";
-    openBtn.style.opacity = "0";
+    openBtn.setAttribute("onclick", "navClose();");
+    document.addEventListener("click", function() {
+      navClose();
+    });
   }
-  
-  function closeNav() {
-    nav.style.opacity = "0";
-    openBtn.style.opacity = "1";
+}
+
+function navClose() {
+  if (nav.style.opacity = "1") {
+    document.addEventListener("click", function() {
+      nav.style.opacity = "0";
+      openBtn.setAttribute("onclick", "navView();");
+    });
+    
   }
+}
