c61b6aa4e618377494d967b4a9a6c65e1f2b8efd
andrew <andrewnijmeh1@gmail.com>
2020-09-21 15:56:27 -0400

    avatar command

---
+++ cogs/avatar.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+class Avatar(commands.Cog):
+    def __init__(self, bot):
+        self.bot_check
+
+    
+
+    @commands.commands(alias=["av"])
+    async def avatar(self, ctx, *, user: discord.Member):
+        if not user:
+            user = ctx.author
+        if ctx.guild:
+            member = ctx.guild.get_member(user.id)
+        
+        try:
+            await ctx.send(embed=discord.Embed(
+                color=member.color if member else ctx.author.color
+
+            ).set_image(url=str(user.avatar_url_as(static_format='png', size=2048))
+            ))
+
+        except Exception:
+            pass
+        
+        except Exception:
+            pass
+
+def setup(bot):
+    bot.add_cog(Avatar(bot))
+    print(f"Cog Avatar Loaded Sucessfully.")
--- cogs/mod.py
+++ cogs/mod.py
@@ -1,13 +1,17 @@
 import discord
 from discord.ext import commands
-from discord.ext.errors import *
+from discord import utils
 import asyncpg
 import asyncio
 import re
 
 
 class Moderation(commands.Cog, name="Mod commands"):
-    """commands used to moderatore your guild"""
 
     def __init__(self, bot):
         self.bot
+
+
+    async def mute(self, ctx, member: discord.Member):
+        pass
+        
