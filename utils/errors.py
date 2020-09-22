ccdfb9ef323291b6053a1cb837924ac71707ff19
andrew <andrewnijmeh1@gmail.com>
2020-09-22 17:57:35 -0400

    added my own exceptions defnitley will add more when needed..

---
+++ utils/errors.py
@@ -0,0 +1,16 @@
+
+
+class OptionConfigErorr(Exception):
+    def __init__(self, *args):
+        self.config = args[0]
+
+    def __str__(self):
+        return f'{self.config} has an invalid config'
+
+# Used when a module/cog is not found in local directores
+class InvalidModuleErorr(Exception):
+    def __init__(self, *args):
+        self.module = args[0]
+
+    def __str__(self):
+        return f'{self.module} is invalid. and or does not exist'
