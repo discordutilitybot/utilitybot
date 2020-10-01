dc0243692710dd6ee3cfb2e2f0ef2a4b206cba69
andrew <andrewnijmeh1@gmail.com>
2020-10-01 17:32:17 -0400

    added primary kreys for guild settings  and leave channel

--- plugins/database/models/database.sql
+++ plugins/database/models/database.sql
@@ -4,5 +4,10 @@ CREATE TABLE guilds (
 )
 
 CREATE TABLE guild_settings (
+    prefix VARCHAR(15) DEFAULT 'u!',
+    log_channel VARCHAR(20),
+    join_channel VARCHAR(20) 
+    leave_channel VARCHAR(20)
+    PRIMARY KEY (prefix, log_channel, join_channel, leave_channel)
 
 )
