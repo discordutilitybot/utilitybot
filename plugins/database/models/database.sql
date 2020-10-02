f790a88cd58d47cf5329b43c4ae627e2a316a12c
andrew <andrewnijmeh1@gmail.com>
2020-10-02 06:52:18 -0400

    switched to different volume

--- plugins/database/models/database.sql
+++ plugins/database/models/database.sql
@@ -7,7 +7,7 @@ CREATE TABLE guild_settings (
     guild_prefix VARCHAR(15) DEFAULT 'u!',
     log_channel INT(20) DEFAULT NULL,
     join_channel INT(20) DEFAULT NULL,
-    leave_channel INT(20)  DEFAULT NULL,
+    leave_channel INT(20) DEFAULT NULL,
     PRIMARY KEY (guild_prefix, log_channel, join_channel, leave_channel)
 
 )
