6792ea1c10e28ae1bacd057f6c5406de6147790b
andrew <andrewnijmeh1@gmail.com>
2020-10-04 10:44:07 -0400

    spelling changes

--- plugins/database/models/database.sql
+++ plugins/database/models/database.sql
@@ -6,11 +6,11 @@ CREATE TABLE guilds (
 )
 
 CREATE TABLE guild_settings (
-    mute_role INT(25) DEAULT 'Muted'
+    muted_role INT(25) DEAULT 'Muted'
     guild_prefix VARCHAR(15) DEFAULT 'u!',
     log_channel INT(20) DEFAULT NULL,
     join_channel INT(20) DEFAULT NULL,
     leave_channel INT(20) DEFAULT NULL,
-    PRIMARY KEY (guild_prefix, log_channel, join_channel, leave_channel)
+    PRIMARY KEY (log_channel, join_channel, leave_channel)
 
 )
