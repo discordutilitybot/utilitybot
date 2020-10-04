6581bfdaab114f7ac3fc3e9abe0f48fe93936084
andrew <andrewnijmeh1@gmail.com>
2020-10-04 10:30:07 -0400

    finished  invite command ill add the invite later in production

--- cogs/invite.py
+++ cogs/invite.py
@@ -1,4 +1,15 @@
 """Invite Link for the bot"""
 import discord
-from discord.ext import command
+from discord.ext import commands
+from utilitybot import db 
 
+class Invite(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    
+
+
+    @commands.command()
+    async def invite(self, *, bot):
+        await ctx.send("Here is the invite link for utilitybot:")
