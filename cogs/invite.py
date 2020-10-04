9cf74f3942de08c70837eddb5bc9a3a8f81d514c
andrew <andrewnijmeh1@gmail.com>
2020-10-04 10:30:36 -0400

    remove crappy import

--- cogs/invite.py
+++ cogs/invite.py
@@ -1,15 +1,13 @@
 """Invite Link for the bot"""
 import discord
 from discord.ext import commands
-from utilitybot import db 
+
 
 class Invite(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     
-
-
     @commands.command()
     async def invite(self, *, bot):
-        await ctx.send("Here is the invite link for utilitybot:")
+        await ctx.send("Here is the invite link for utilitybot: ")
