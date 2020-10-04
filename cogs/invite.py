613121d25670137402cbe507f510896f94c4128c
andrew <andrewnijmeh1@gmail.com>
2020-10-04 10:31:45 -0400

    addewd commands aliases

--- cogs/invite.py
+++ cogs/invite.py
@@ -8,6 +8,6 @@ class Invite(commands.Cog):
         self.bot = bot
 
     
-    @commands.command()
+    @commands.command(aliases=['inv, botinvite'])
     async def invite(self, *, ctx, bot):
         await ctx.send("Here is the invite link for utilitybot: ")
