e021dcecf7c62e02a693cd367de07d7376cd7fb0
andrew <andrewnijmeh1@gmail.com>
2020-10-05 16:11:41 -0400

    bot info cog

---
+++ cogs/botinfo.py
@@ -0,0 +1,3 @@
+import discord
+from discord.ext import commands
+from dsicord.utils import get
--- utilitybot-dev.py
+++ utilitybot-dev.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 
 import secrets
 from secrets import *
-
 bot = commands.Bot(command_prefix="u!")
 
 for filename in os.listdir('./cogs'):
