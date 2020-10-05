e538f48ac7d8334656b7867ab743809641698ef9
andrew <andrewnijmeh1@gmail.com>
2020-10-05 16:19:04 -0400

    ill add setup fucntion an instance vars for noow ill get done later

--- cogs/botinfo.py
+++ cogs/botinfo.py
@@ -1,3 +1,11 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
+
+class Botinfo(commands.Cog):
+    def __init__(self, bot):
+        self.bot
+
+
+def setup(bot):
+    bot.add_cog(Botinfo(bot))
