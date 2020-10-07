c6072927da4535cfb792d3e2791bffb8c3eb7c78
andrew <andrewnijmeh1@gmail.com>
2020-10-07 16:07:44 -0400

    big int not null for guold settings cand channel id

--- plugins/database/models/database.sql
+++ plugins/database/models/database.sql
@@ -9,7 +9,7 @@ CREATE TABLE guild_settings (
     guild_prefix VARCHAR(15) DEFAULT 'u!',
     log_channel INT(20) DEFAULT NULL,
     join_channel INT(20) DEFAULT NULL,
-    leave_channel INT(20) DEFAULT NULL,
+    leave_channel BIGINT NOT NULL DEFAULT NULL,
     PRIMARY KEY (log_channel, join_channel, leave_channel)
 
 )
