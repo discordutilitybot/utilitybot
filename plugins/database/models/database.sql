3136fb7eb7c83c3c675bd496fed3f5998e2a6185
andrew <andrewnijmeh1@gmail.com>
2020-10-07 16:05:45 -0400

    finally i can commit /models stuff

---
+++ plugins/database/models/database.sql
@@ -0,0 +1,16 @@
+/* This is were we collect global info of each guild e.g: messages, role count, channel count,guild_id*/
+CREATE TABLE guilds (
+    
+    guild_id BIGINT NOT NULL
+    
+)
+
+CREATE TABLE guild_settings (
+    muted_role INT(20) DEFAULT 'Muted',
+    guild_prefix VARCHAR(15) DEFAULT 'u!',
+    log_channel INT(20) DEFAULT NULL,
+    join_channel INT(20) DEFAULT NULL,
+    leave_channel INT(20) DEFAULT NULL,
+    PRIMARY KEY (log_channel, join_channel, leave_channel)
+
+)
