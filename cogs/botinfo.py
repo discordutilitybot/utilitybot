b426eec0c0d0d361c3d9501542e0c507dcfdcf69
andrew <andrewnijmeh1@gmail.com>
2020-10-08 07:27:43 -0400

    added aliases for bot infoc ommand

--- cogs/botinfo.py
+++ cogs/botinfo.py
@@ -6,8 +6,11 @@ class Botinfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def botinfo(self, ctx)    
+    @commands.command(aliases=['infobot, info'])
+    async def botinfo(self, ctx):
+        embed = discord.Embed(
+            title = "All about utility bot"
+        )   
 
 def setup(bot):
     bot.add_cog(Botinfo(bot))
