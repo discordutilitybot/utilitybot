28e4eec90ecc6b3ff5a0b21a0a402c29932df09a
andrew <andrewnijmeh1@gmail.com>
2020-10-10 19:22:48 -0400

    aded reference comment

--- utils/permissions.py
+++ utils/permissions.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 
+"""Simple function to check whether the bot has permissions to perform events or actions."""
 def has_permission(permission : str, **kwargs):
     def predicate(ctx):
         if ctx.bot.has_permission(ctx.author, permission):
