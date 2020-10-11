9eed6f7f3f9d6a15d01acc4eae04d73d3dd2c35c
andrew <andrewnijmeh1@gmail.com>
2020-10-11 16:11:09 -0400

    i think i can now use u!avatar to get my own avatar without getting my personel id

--- cogs/avatar.py
+++ cogs/avatar.py
@@ -11,16 +11,16 @@ class Avatar(commands.Cog):
     async def avatar(self, ctx, *, user: discord.Member):
         if not user:
             user = ctx.author
-
         if ctx.guild:
             member = ctx.guild.get_member(user.id)
-        
-        
         await ctx.send(embed=discord.Embed(
             color=member.color if member else ctx.author.color
+        ).set_image(
+            url=str(user.avatar_url_as(static_format='png', size=2048))
+        ))
 
-            ).set_image(url=str(user.avatar_url_as(static_format='png', size=2048))
-            ))
+
+       
 
 def setup(bot):
     bot.add_cog(Avatar(bot))
