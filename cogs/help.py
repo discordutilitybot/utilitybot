8806c75508964b947704dcb6617422bb020049eb
andrew <andrewnijmeh1@gmail.com>
2020-10-11 14:03:09 -0400

    remove var that was not needed and would be unused

--- cogs/help.py
+++ cogs/help.py
@@ -13,19 +13,17 @@ class Help(commands.Cog):
     """Global help command (there will be help commands for each module)"""
 
     async def help(self, ctx):
-         prefix = ctx.prefix.replace(
-            '<@444871677176709141> ', '@Fire ').replace('<@!444871677176709141>', '@Fire ')
         embed = discord.Embed(colour=ctx.author.color, description=f'''Here\'s some helpful links
-[Commands](websiteredirecthere)
-[Support Server](sereverlinkhere)
-[Invite Me](invitelinkhere)
-''')
+    [Commands](websiteredirecthere)
+    [Support Server](sereverlinkhere)
+    [Invite Me](invitelinkhere)
+        ''')
 
             
 
         
 
 def setup(bot):
-    bot.add_help = bot.remove_command("help")
+    bot.remove_command("help")
     bot.add_cog(Help(bot))
 
