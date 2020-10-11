0e79e387aafdae684b300d3377fd824b0df0509a
andrew <andrewnijmeh1@gmail.com>
2020-10-11 09:46:00 -0400

    delete

--- cogs/help.py
+++ cogs/help.py
@@ -11,20 +11,7 @@ class Help(commands.Cog):
     """Global help command (there will be help commands for each module)"""
 
     async def help(self, ctx):
-        if module_name is None:
-            embed = discord.Embed(
-                title = "Modules",
-                description = "List of all the modules you can find!",
-                colour = discord.Colour.blue()
-            )
         
-            embed.add_field(name=":tada: Fun Commands", value="Fun commands", inline=False)
-            embed.add_field(name=":frame_photo: Images", value="Image commands ", inline=False) 
-            embed.add_field(name=":wrench:, Utility", value="utility commands", inline=False)
-            embed.add_field(name=":information_source :Info", value="u!info")
-
-        
-            await ctx.send(embed=embed)
         
 
         
