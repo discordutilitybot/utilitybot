61f32cc00d70f696e2a41c35171ae78be07d569e
andrew <andrewnijmeh1@gmail.com>
2020-10-13 16:50:12 -0400

    remove unused imports

--- website/src/backend/setup.py
+++ website/src/backend/setup.py
@@ -1,6 +1,5 @@
 import flask
 from flask import Flask
-from flask import Blueprint, redirect, render_template, url_for, session
-from requests_oauthlib import OAuth2Session
+from flask import Blueprint, redirect, render_template, session
 import requests
 
