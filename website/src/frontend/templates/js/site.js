1e86670e4c2d623fc9921bb5482be93393d0e40a
PineappleRind <pineapplerind.info@gmail.com>
2020-10-14 12:31:48 -0400

    YESSS commands list fixed

--- website/src/frontend/templates/js/site.js
+++ website/src/frontend/templates/js/site.js
@@ -2,75 +2,32 @@ let nav = document.getElementById("navCollapsed"); // The navbar
 let openBtn = document.getElementById('openButton'); // The trigger
 let navContents = document.getElementById('navList'); // The navbar contents
 
+let main = document.getElementById('main'); // the main page to be 'pushed' to the right
+let footer = document.getElementById('foot') // the footer will be 'pushed' also
 function navView() { // Open nav function
-  if (nav.style.height = "0%") { // If it is invisible,
-    nav.style.height = "100%"; // Make it visible
+  if (nav.style.width = "0px") { // If it is invisible,
+    nav.style.width = "350px"; // Make it visible
     openBtn.setAttribute("onclick", "navClose();"); // Sets button to close navbar function
-    navContents.style.animationName = 'float-right';
-    navContents.style.display = 'block'; // Nav contents made visible on opening nav
+    setTimeout(function(){navContents.style.animationName = 'float-right'; navContents.style.display = 'block';}, 700); // Loading animation for nav contents
+    main.style.marginLeft = '350px';
+    footer.style.marginLeft = '350px'; // 'Pushes' items to right
   }
 }
 
 function navClose() { // Close navbar function
-  if (nav.style.height = "100%") { // If it's visible,
-    nav.style.height = "0%"; // Make it invisible
+  if (nav.style.width = "350px") { // If it's visible,
+    nav.style.width = "0px"; // Make it invisible
     openBtn.setAttribute("onclick", "navView();") // Sets button to open nav function
-    setTimeout(function(){navContents.style.display = 'none';}, 700) // Nav contents made invisible after 0.7s of closing nav
+    setTimeout(function(){navContents.style.display = 'none';}, 700); // Nav contents made invisible after 0.7s of closing nav
+    main.style.marginLeft = '0';
+    footer.style.marginLeft = '0';
   }
 }
 
-/* This next bit of code is a last resort so it's HORRIBLE. T-T */
-function infoCommands() {
-  if (document.getElementById('infoCommands').style.maxHeight = '29px') {
-    document.getElementById('infoCommands').style.maxHeight = '600px';
-    document.getElementById('infoCommands').setAttribute("onclick", "hideInfoCommands()");
-  } 
-}
-function hideInfoCommands() {
-  document.getElementById('infoCommands').style.maxHeight = '29px';
-  document.getElementById('infoCommands').setAttribute("onclick", "infoCommands()");
-}
-
-function utilCommands() {
-  if (document.getElementById('utilCommands').style.maxHeight = '29px') {
-    document.getElementById('utilCommands').style.maxHeight = '150px';
-    document.getElementById('utilCommands').setAttribute("onclick", "hideUtilCommands()");
-  } 
-}
-function hideUtilCommands() {
-  document.getElementById('utilCommands').style.maxHeight = '29px';
-  document.getElementById('utilCommands').setAttribute("onclick", "utilCommands()");
-}
-
-function modCommands() {
-  if (document.getElementById('modCommands').style.maxHeight = '29px') {
-    document.getElementById('modCommands').style.maxHeight = '600px';
-    document.getElementById('modCommands').setAttribute("onclick", "hideModCommands()");
-  } 
-}
-function hideModCommands() {
-  document.getElementById('modCommands').style.maxHeight = '29px';
-  document.getElementById('modCommands').setAttribute("onclick", "modCommands()");
-}
-
-function funCommands() {
-  if (document.getElementById('funCommands').style.maxHeight = '29px') {
-    document.getElementById('funCommands').style.maxHeight = '600px';
-    document.getElementById('funCommands').setAttribute("onclick", "hideFunCommands()");
-  } 
-}
-function hideFunCommands() {
-  document.getElementById('funCommands').style.maxHeight = '29px';
-  document.getElementById('funCommands').setAttribute("onclick", "funCommands()");
-}
-
-function openInNewTab(url) {
+function openInNewTab(url) { // Opens oauth link by clicking on <li>
   var win = window.open(url, '_blank');
   win.focus();
 }
-
-/* The following code is based off of: https://bit.ly/32C1vfo
-Comments added by me. */
 
 let acc = document.getElementsByClassName("faq-question"); // accordion variable
 
@@ -83,4 +40,16 @@ for (let i = 0; i < acc.length; i++) {
             content.style.maxHeight = content.scrollHeight + "px"; // If it's null, make it not null
         }
     }
+}
+
+let commandsTitle = document.getElementsByClassName("commands-title");
+
+for (let i = 0; i < commandsTitle.length; i++) {
+  commandsTitle[i].onclick = function() { // When click is detected execute the following
+    if (this.style.maxHeight != '600px') { // if the table is not open,
+          this.style.maxHeight = '600px'; // open it
+    } else { // if it's not closed,
+      this.style.maxHeight = '29px'; // close it
+    }
+  }
 }
