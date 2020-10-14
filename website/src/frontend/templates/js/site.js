a123e9502cabb47303b2da0e2d8a7cd715298a2b
PineappleRind <pineapplerind.info@gmail.com>
2020-10-14 19:57:04 -0400

    Better animation

--- website/src/frontend/templates/js/site.js
+++ website/src/frontend/templates/js/site.js
@@ -1,3 +1,5 @@
+/* JavaScript by PineappleRind (https://github.com/pineapplerind) */
+
 let nav = document.getElementById("navCollapsed"); // The navbar
 let openBtn = document.getElementById('openButton'); // The trigger
 let navContents = document.getElementById('navList'); // The navbar contents
@@ -7,8 +9,9 @@ let footer = document.getElementById('foot')
 function navView() { // Open nav function
   if (nav.style.width = "0px") { // If it is invisible,
     nav.style.width = "350px"; // Make it visible
+    navContents.style.display = 'block';
     openBtn.setAttribute("onclick", "navClose();"); // Sets button to close navbar function
-    setTimeout(function(){navContents.style.animationName = 'float-right'; navContents.style.display = 'block';}, 700); // Loading animation for nav contents
+    navContents.style.animationName = 'float-right'; 
     main.style.marginLeft = '350px';
     footer.style.marginLeft = '350px';
   }
@@ -16,11 +19,14 @@ function navView() { // Open nav function
 
 function navClose() { // Close navbar function
   if (nav.style.width = "350px") { // If it's visible,
-    nav.style.width = "0px"; // Make it invisible
+    setTimeout(function(){
+      nav.style.width = "0px"; // Make it invisible
+      main.style.marginLeft = '0';
+      footer.style.marginLeft = '0';
+      navContents.style.display = 'none';
+    },1000)
     openBtn.setAttribute("onclick", "navView();") // Sets button to open nav function
-    setTimeout(function(){navContents.style.display = 'none';}, 700); // Nav contents made invisible after 0.7s of closing nav
-    main.style.marginLeft = '0';
-    footer.style.marginLeft = '0';
+    navContents.style.animationName = 'float-left';
   }
 }
 
@@ -43,7 +49,6 @@ for (let i = 0; i < acc.length; i++) {
 }
 
 let commandsTitle = document.getElementsByClassName("commands-title"); // accordion variable
-
 for (let i = 0; i < commandsTitle.length; i++) {
   commandsTitle[i].onclick = function() { // When click is detected execute the following // The accordion content
     if (this.style.maxHeight != '600px') {
