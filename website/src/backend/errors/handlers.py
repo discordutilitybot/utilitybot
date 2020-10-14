a44d94014ce382939d84eb167f0415e9168a7ddd
andrew <andrewnijmeh1@gmail.com>
2020-10-14 07:18:20 -0400

    pass proper path

--- website/src/backend/errors/handlers.py
+++ website/src/backend/errors/handlers.py
@@ -4,3 +4,7 @@ from flask import Flask, Blueprint, render_template
 
 """Initialise a blueprint"""
 errors =  Blueprint('errors', __name__)
+
+@errors.app_errorhandler(404)
+def error_404(error):
+    return render_template('website/src/frontend/templates/404.html'), 404
