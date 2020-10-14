ac0ed5c5b03377cdcd65a4da4a942d6adcfbbd86
andrew <andrewnijmeh1@gmail.com>
2020-10-14 15:34:26 -0400

    update directory when returning a template for our 403 and 404 page

--- website/src/backend/errors/handlers.py
+++ website/src/backend/errors/handlers.py
@@ -7,8 +7,12 @@ errors =  Blueprint('errors', __name__)
 
 @errors.app_errorhandler(404)
 def error_404(error):
-    return render_template('website/src/frontend/templates/404.html'), 404
+    return render_template('website/src/backend/errors/404.html'), 404
 
 @errors.app_errorhandler(403)
 def error_403(error):
-    return render_template('website/src/frontend/templates/404.html'), 403
+    return render_template('website/src/backend/errors/403.html'), 403
+
+@errors.app_errorhandler(401)
+def error_401():
+    return render_template(''), 401
