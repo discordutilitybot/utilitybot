ec8b189b3abbb3567cf1847d88cca896ed59fcdc
andrew <andrewnijmeh1@gmail.com>
2020-10-14 07:17:26 -0400

    import render_template

--- website/src/backend/errors/handlers.py
+++ website/src/backend/errors/handlers.py
@@ -1,5 +1,6 @@
 import flask
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, render_template
 """Custom error pages using the extending home.html"""
 
+"""Initialise a blueprint"""
 errors =  Blueprint('errors', __name__)
