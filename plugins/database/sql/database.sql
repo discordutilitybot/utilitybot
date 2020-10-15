c360f0542f43a360139166ad4e6f0d9deea85193
andrew <andrewnijmeh1@gmail.com>
2020-10-15 07:22:43 -0400

    update sql

--- plugins/database/sql/database.sql
+++ plugins/database/sql/database.sql
@@ -1,6 +1,7 @@
 
 CREATE TABLE guilds (
     guild_id INT(20) NOT NULL DEFAULT 0,
+    /* Amount of roles the guild has not the role id's...*/
     guild_roles BIGINT NOT NULL DEFAULT 0,
     guild_channels BIGINT NOT NULL DEFAULT 0,
     guild_messages BIGINT NOT NULL DEFAULT 0,
