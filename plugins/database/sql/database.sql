31b4725ba14da9894b62c8664ebe3a99b473f3c5
andrew <andrewnijmeh1@gmail.com>
2020-10-15 07:18:47 -0400

    add logging and moderation column

--- plugins/database/sql/database.sql
+++ plugins/database/sql/database.sql
@@ -13,7 +13,8 @@ CREATE TABLE guild_settings (
     muted_role INT(20) DEFAULT 'Muted',
     guild_prefix VARCHAR(15) DEFAULT 'u!',
     /* These are different types of logging you can have moderation logs e.g: a ban log or just action logs..*/
-
+    logging_moderation BIGINT DEFAULT NULL,
+    logging_action BIGINT DEFAULT NULL
     PRIMARY KEY (muted_role, guild_prefix,)
     
 )
