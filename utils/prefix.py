325f54482b1bee6e8052684af52ebae12557a801
andrew <andrewnijmeh1@gmail.com>
2020-10-23 16:40:06 -0400

    get_prefix function done

--- utils/prefix.py
+++ utils/prefix.py
@@ -2,4 +2,7 @@ import discord
 from discord import Guild
 from discord.ext import commands
 
+
 # Make sure its always asynchronous
+async def get_prefix(bot, message):
+    prefix = await (await bot.db.execute("SELECT guild_prefix FROM guilds WHERE id = ?", message.guild.id)).fetchone()
