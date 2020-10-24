7142b2f3466b451764e59e569d3d3e1f1772cec6
andrew <andrewnijmeh1@gmail.com>
2020-10-24 14:08:01 -0400

    create ping class

--- cogs/dstatus.py
+++ cogs/dstatus.py
@@ -1,3 +1,11 @@
 """Used to check the ping of the bot"""  
 import discord
 from discord.ext import commands
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(Ping(bot))
