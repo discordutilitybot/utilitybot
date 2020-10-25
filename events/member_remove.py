1266eaed805a808dc461943130e5cf462d939a8c
andrew <andrewnijmeh1@gmail.com>
2020-10-25 10:25:20 -0400

    update

--- events/member_remove.py
+++ events/member_remove.py
@@ -5,7 +5,10 @@ class MemberRemove(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
+    @commands.Cog.listener()
+    """For kick, ban it will be more specific in the log this is just a global event for kick ban etc"""
+    async def on_member_remove(self, member):
+        pass
 
 
 def setup(bot):
