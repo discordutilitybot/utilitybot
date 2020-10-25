8ed4a523b677199dc9c87099549a2a1e3611261e
andrew <andrewnijmeh1@gmail.com>
2020-10-25 10:14:50 -0400

    add setup function for memberremove

--- events/member_remove.py
+++ events/member_remove.py
@@ -1,2 +1,12 @@
 import discord
 from discord.ext import commands
+
+class MemberRemove(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+
+
+def setup(bot):
+    bot.add_cog(MemberRemove(bot))
