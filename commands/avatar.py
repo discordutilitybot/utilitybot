a4e2a050f4daee5e0586a0bb8f69aa13222163f8
andrew <andrewnijmeh1@gmail.com>
2020-10-26 15:39:44 -0400

    created /commands since a cog is technically a class of commands and these are just 1 command..

---
+++ commands/avatar.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+class Avatar(commands.Cog):
+    def __init__(self, bot):
+        self.bot_check
+
+    
+
+    @commands.command(alias=["av"])
+    async def avatar(self, ctx, *, user: discord.Member):
+        
+        if not user:
+            user = ctx.author
+            
+        member = None
+        if ctx.guild:
+            member = ctx.guild.get_member(user.id)
+        await ctx.send(embed=discord.Embed(
+            color=member.color if member else ctx.author.color
+        ).set_image(
+            url=str(user.avatar_url_as(static_format='png', size=2048))
+        ))
+
+       
+
+def setup(bot):
+    bot.add_cog(Avatar(bot))
+    print(f"Cog Avatar Loaded Sucessfully.")
