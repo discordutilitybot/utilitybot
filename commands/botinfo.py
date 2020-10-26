71548d4ec0d205b93352641cb9230db91f15766a
andrew <andrewnijmeh1@gmail.com>
2020-10-26 18:39:06 -0400

    add some stuff.

--- commands/botinfo.py
+++ commands/botinfo.py
@@ -9,10 +9,11 @@ class Botinfo(commands.Cog):
     @commands.command(aliases=['infobot, info'])
     async def botinfo(self, ctx):
         embed = discord.Embed(
-            title = "All about utility bot",
-            description = "Something here",
-            color = discord.Color.blue()
+            title = "All about Utility Bot!",
+            description = "Hello! I am Utility Bot I'm here to help you manage and moderate your server with a touch of fun! I was written in Python by Flop#7234 using discord.py! Type u!help to get a link to our website for a list of commands!",
+            color = discord.Color.orange()
         )   
+        
         
         embed.set_thumbnail(url="https://discord.com/channels/@me/700807345630085201/767113082446807070")
         embed.set_footer(text=f"Guilds: {len(self.bot.guilds)}")
