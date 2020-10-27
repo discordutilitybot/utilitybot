21193df1ce1e1df3da734f479162b397e359ac80
andrew <andrewnijmeh1@gmail.com>
2020-10-27 07:17:38 -0400

    i should use setup function with cogs for setup in unitest.testcase

--- tests/test_bot_info.py
+++ tests/test_bot_info.py
@@ -6,8 +6,9 @@ from discord.ext import commands
 class TestBotInfo(unittest.TestCase):
 
     def setUp(self):
-        self.app = bot_info.test_client()
+        self.app = bot_info.app.setup()
         self.app.testing = True
     
     # Tests main functionality of the command :)
     def test_bot_info(self):
+        self.
