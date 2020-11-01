a9375617f297489d5df79d308b3158be18f57a11
andrew <andrewnijmeh1@gmail.com>
2020-11-01 12:21:40 -0500

    syntax remove colon

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -71,9 +71,9 @@ class Moderation(commands.Cog, name="Moderation"):
         )
         
         # I believe we should add roles before saying the person was muted :)
-        await member.add_roles(muted):
+        await member.add_roles(muted)
         # Global
-        
+
         await ctx.send(f"{member.mention} was muted for {reason}")
         
         # Send a dm to a member saying they were muted
