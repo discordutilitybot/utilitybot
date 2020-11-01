ffe300d7fcf2719e8058c43545df7e9036370058
andrew <andrewnijmeh1@gmail.com>
2020-11-01 12:16:21 -0500

    send a dm to a muted user

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -70,6 +70,12 @@ class Moderation(commands.Cog, name="Moderation"):
             #ctx.guild.roles, name="Muted"
         )
         
+        # I believe we should add roles before saying the person was muted :)
+        await member.add_roles(muted):
+        # Global
+        await ctx.send(f"{member.mention} was muted for {reason}")
+        # Send a dm to a member saying they were muted
+        await member.send(f"You were muted in {ctx.guild.name} for {reason}")
     
     @commands.has_permissions(mute_members=True)
     @commands.command()
