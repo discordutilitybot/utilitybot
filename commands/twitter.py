2f9ad88f4ee04802a3b611889e64491c65a50850
andrew <andrewnijmeh1@gmail.com>
2020-11-01 10:38:24 -0500

    finish command :)

--- commands/twitter.py
+++ commands/twitter.py
@@ -2,10 +2,14 @@ import discord
 from discord.ext import commands
 
 
+"""Get a link to utility bot's twitter"""
 class Twitter(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def twitter(self, ctx):
+        await ctx.send("Here's a link to utility bot's twitter \n [twitter]())
 
 
 def setup(bot):
