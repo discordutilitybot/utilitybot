89d2a46228542217425cad8ae90193b0841136b0
andrew <andrewnijmeh1@gmail.com>
2020-11-03 07:32:10 -0500

    use cool gif

--- events/member_remove.py
+++ events/member_remove.py
@@ -13,20 +13,23 @@ class MemberRemove(commands.Cog):
             moderator = None
             action = None
             reason = None
-        if member.guild.me.guild_permissions.view_audit_log:
-            async for e in member.guild.audit_logs(limit=5):
-                if e.action in [discord.AuditLogAction.kick, discord.AuditLogAction.ban] and e.target.id == member.id:
-                    moderator = e.user
-                    if moderator == member.guild.me:
+            if member.guild.me.guild_permissions.view_audit_log:
+                async for e in member.guild.audit_logs(limit=5):
+                    if e.action in [discord.AuditLogAction.kick, discord.AuditLogAction.ban] and e.target.id == member.id:
+                        moderator = e.user
+                        if moderator == member.guild.me:
                         moderator = None
                         break
-                if e.action == discord.AuditLogAction.kick:
+
+                    if e.action == discord.AuditLogAction.kick:
                     action = 'kick'
                     reason = e.reason
-                if e.action = discord.AuditLogAction.ban:
+
+                    if e.action = discord.AuditLogAction.ban:
                     action = 'ban'
                     reason = e.reason
                 break
+        embed = discord.Embed(title="Member Left", url="https://tenor.com/view/bear-hug-wave-bye-gif-12388210")
 
 
 
