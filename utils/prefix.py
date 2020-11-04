e9eefea9da8f4f9f742f7995a1919f4efe41fb12
andrew <andrewnijmeh1@gmail.com>
2020-11-04 15:05:47 -0500

    use ctx

--- utils/prefix.py
+++ utils/prefix.py
@@ -4,5 +4,5 @@ from discord.ext import commands
 
 
 # Make sure its always asynchronous (since dpy is async duh)
-async def get_prefix(bot, message):
+async def get_prefix(ctx, bot, message):
     prefix = await (await bot.db.execute(f"SELECT guild_prefix FROM guilds WHERE id = {ctx.guild.id}", message.guild.id)).fetchone()
