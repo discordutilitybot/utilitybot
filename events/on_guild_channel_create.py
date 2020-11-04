9831505b6920ba0fcb9cf0b223f64afddb5174f4
andrew <andrewnijmeh1@gmail.com>
2020-11-04 15:25:15 -0500

    fix auditlogchannel limit=3

--- events/on_guild_channel_create.py
+++ events/on_guild_channel_create.py
@@ -10,8 +10,14 @@ class ChannelCreate(commands.Cog):
         actionlogch = self.bot.db.execute(f"SELECT logging_action FROM guild_settings WHERE id = {ctx.guild.id}")
 
         if actionlogch:
-            pass
+            createdby =  None
+            if channel.guild.me.guild_permissions.view_audit_log:
+                async for e in channel.guild.audit_logs(action=discord.AuditLogAction.channel_create, limit=3):
+                    if e.target.id == channel.id:
+                        createdby = e.user
+                        break
 
 
+        
 def setup(bot):
     bot.add_cog(ChannelCreate(bot))
