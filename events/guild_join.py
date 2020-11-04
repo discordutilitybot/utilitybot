8efeca2a97c3e2bc7a6026c5c1305bf0f292401d
andrew <andrewnijmeh1@gmail.com>
2020-11-04 15:41:05 -0500

    more licesning ;-;

--- events/guild_join.py
+++ events/guild_join.py
@@ -1,3 +1,25 @@
+"""MIT License
+
+Copyright (c) 2020 utilitybot.co
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
 import discord
 from discord.ext import commands
 from discord import Guild, User
@@ -5,8 +27,6 @@ from discord.utils import get
 
 import asyncpg
 import logging
-
-
 
 class GuildJoin(commands.Cog):
     def __init__(self, bot):
@@ -22,7 +42,7 @@ class GuildJoin(commands.Cog):
             reason="Utility bot's Default Muted Role on join. (used for muting)",
             permissions=permissions,
             color=discord.Color.orange())
-            
+        
 
         
     
