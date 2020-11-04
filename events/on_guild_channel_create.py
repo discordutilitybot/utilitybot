7a218857e75c81b83796ab97cf80d8946bef51fc
andrew <andrewnijmeh1@gmail.com>
2020-11-04 15:11:46 -0500

    create function

--- events/on_guild_channel_create.py
+++ events/on_guild_channel_create.py
@@ -5,7 +5,9 @@ class ChannelCreate(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    
+    @commands.Cog.listener()
+    async def on_guild_channel_create(self, bot):
+        pass
 
 def setup(bot):
     bot.add_cog(ChannelCreate(bot))
