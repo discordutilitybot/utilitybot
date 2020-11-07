dc26c80a3305743672dafac025fc12ae915f3701
andrew <andrewnijmeh1@gmail.com>
2020-11-07 12:54:01 -0500

    delete website folder old-website has the exact files since we have the new repo

--- website/src/backend/blueprints/oauth.py
+++
@@ -1,4 +0,0 @@
-import flask
-from flask import Flask
-from flask import Blueprint, render_template, url_for
-import requests_oauthlib
--- website/src/backend/setup.py
+++
@@ -1,5 +0,0 @@
-import flask
-from flask import Flask
-from flask import Blueprint, redirect, render_template, session
-import requests
-
