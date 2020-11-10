83250c557c0dece168be22dedf0fc075672b6fa5
andrew <andrewnijmeh1@gmail.com>
2020-11-10 15:11:05 -0500

    fix function PEP

--- commands/avatar.py
+++ commands/avatar.py
@@ -47,7 +47,7 @@ class Avatar(commands.Cog):
         if user is None:
            await ctx.send(embed=discord.Embed(
                color=member.color if member else ctx.author.color.set_image(
-                   url=str(user.abatar_url_as(static_format='png', size=1048))
+                   url=str(user.avatar_url_as(static_format='png', size=1048))
                )
             ))
 
