e8a59b75f7704dd8eb3e5cd2df0093fc3282766b
andrew <andrewnijmeh1@gmail.com>
2020-11-12 20:50:37 -0500

    yes

--- events/greetmsg/on_member_join.py
+++ events/greetmsg/on_member_join.py
@@ -7,8 +7,10 @@ class Greetmsg(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        pass
 
 def setup(bot):
     bot.add_cog(Greetmsg(bot))
-    bot.logger.info
+    bot.logger.info("Loaded event Greetmsg!")
