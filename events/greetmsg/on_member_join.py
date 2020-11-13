9020692169c5b9687225b49c84b5f310ae962aa7
andrew <andrewnijmeh1@gmail.com>
2020-11-12 20:51:56 -0500

    remove logging_leave for ggreetmessage

--- events/greetmsg/on_member_join.py
+++ events/greetmsg/on_member_join.py
@@ -9,8 +9,10 @@ class Greetmsg(commands.Cog):
     
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        pass
+        if self.bot.db.execute("SELECT greet_message FROM guild_settings WHERE id = ?"):
+            join_chan = self
+
 
 def setup(bot):
     bot.add_cog(Greetmsg(bot))
-    bot.logger.info("Loaded event Greetmsg!")
+    bot.logger.info("Event Loaded Greetmsg!")
--- plugins/database/sql/database.sql
+++ plugins/database/sql/database.sql
@@ -17,7 +17,6 @@ CREATE TABLE guild_settings (
     logging_moderation BIGINT DEFAULT NULL,
     logging_action BIGINT DEFAULT NULL,
     /* You can toggle join logs but action logs already toggles it on but if you want less logs you can toggle logging_leave or logging_join.*/
-    logging_leave BIGINT DEFAULT NULL,
     logging_join BIGINT DEFAULT NULL,
     greet_message VARCHAR(80) DEFAULT NULL
     PRIMARY KEY (muted_role, guild_prefix, logging_moderation, logging_action, logging_leave, logging_join),
