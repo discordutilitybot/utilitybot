bd6963d51b528b95362f5ed47f39facf40f57e49
andrew <andrewnijmeh1@gmail.com>
2020-12-03 16:20:49 -0500

    Fix event #11"

--- events/Guild/on_guild_role_create.py
+++ events/Guild/on_guild_role_create.py
@@ -10,19 +10,16 @@ class RoleCreate(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_role_create(self, role):
         logch = self.bot.db.execute("SELECT logging_action FROM guild_settings WHERE id = ?")
-
         if logch:
             embed = discord.Embed(color=discord.Color.green(), timestamp=datetime.datetime.now(
-                datetime.utc), description=f'**A new role was create**\m{role.mention}')
+                datetime.timezone.utc), description=f'**A new role was created**\n{role.mention}')
             embed.set_author(name=role.guild.name,
-                        icon_url=str(role.guild.icon_url))
+                             icon_url=str(role.guild.icon_url))
             embed.set_footer(text=f"Role ID: {role.id}")
-            
-
-        try:
-            await logch.send(embed=embed)
-        except Exception:
-            pass
+            try:
+                await logch.send(embed=embed)
+            except Exception:
+                pass
         
 def setup(bot):
     bot.add_cog(RoleCreate(bot))
