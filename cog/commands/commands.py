a9a62f0db5be091c7698c99725fcbf940a68fdb5
andrew <andrewnijmeh1@gmail.com>
2020-12-06 11:56:09 -0500

    start creating cateogrys

--- cog/commands/commands.py
+++ cog/commands/commands.py
@@ -11,8 +11,19 @@ class Commands(commands.Cog):
     @commands.command()
     async def commands(self, ctx, category):
 
+
         if 'error' in category:
             await ctx.send(":x:That category isn't availaible")
+        
+
+        if category == "moderation":
+            color = ctx.author.color
+            embed = discord.Embed(
+                title="Moderation commands",
+                description="Here is a list of all the moderation commands!",
+                color = color
+
+            )
 def setup(bot):
     bot.add_cog(Commands(bot))
     bot.logging.info("Loaded cog Commands")
