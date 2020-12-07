e544f77834a281a33c61969e4fd6a95da6eccb2a
andrew <andrewnijmeh1@gmail.com>
2020-12-07 15:58:43 -0500

    Player isnt a valid suser

---
+++ cog/hypixel/duels.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import aiohttp
+import datetime
+class Duels(commands.Cog):
+    def __init__(self, bot):
+        self.bot  = bot
+
+    @commands.command()
+    async def duels(self, ctx, user: str):
+        channel = ctx.message.channel
+        async with channel.typing():
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f'https://api.slothpixel.me/api/guilds/{user}') as resp:
+                    player = await resp.json()
+                    color = ctx.author.color
+                if 'error' in player:
+                    embed = discord.Embed(title=f"{player is not a player", colour=color,
+                                                  timestamp=datetime.datetime.utcnow())
+                    await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Duels(bot))
+    bot.logging.info("Loaded duels command")
