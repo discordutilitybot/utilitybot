9527f0c6d4062498159542793f930eb9a1af6e0a
andrew <andrewnijmeh1@gmail.com>
2020-12-07 16:02:35 -0500

    finsusg up duels command

--- cog/hypixel/duels.py
+++ cog/hypixel/duels.py
@@ -12,15 +12,23 @@ class Duels(commands.Cog):
         async with channel.typing():
             async with aiohttp.ClientSession() as session:
                 async with session.get(f'https://api.slothpixel.me/api/guilds/{user}') as resp:
-                    user = await resp.json()
+                    player = await resp.json()
                     color = ctx.author.color
 
                 if 'error' in user:
                     embed = discord.Embed(title=f"{user} is not a player", colour=color,
                                                   timestamp=datetime.datetime.utcnow())
+                    await ctx.send(embed=embed)     
+                else:
+                    embed = discord.Embed(title=f'{user} Duel stats', colour=color, timestamp=datetime.datetime.utcnow())
+                    embed.add_field(name="Wins:", value=player["stats"]["Duels"]["wins"], inline=False)
+                    embed.add_field(name="Kills:", value=player["stats"]["Duels"]["kills"], inline=False)
+                    embed.add_field(name="Deaths:", value=player["stats"]["Duels"]["deaths"], inline=False)
+                    embed.add_field(name="Bow hits:", value=player["stats"]["Duels"]["bow_hits"], inline=False)
+                    embed.add_field(name="Melee hits:", value=player["stats"]["Duels"]["melee_hits"], inline=False)
+                    embed.add_field(name="Best winstreak:", value=player["stats"]["Duels"]["best_overall_winstreak"],
+                                    inline=False)
                     await ctx.send(embed=embed)
-
-
 def setup(bot):
     bot.add_cog(Duels(bot))
     bot.logging.info("Loaded duels command")
