e63f3b2ae82d896853230d8229b311f0c4d1d7c4
TheCodingGuru <akcoding@outlook.com>
2020-12-09 20:12:38 +0000

    channel lock system. flop look at this, im unsure

---
+++ cog/serverSafety/lock.py
@@ -0,0 +1,34 @@
+import discord
+import re 
+from discord.ext import commands
+
+
+
+class LockCog(commands.Cog, name="serverChannelsLock"):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+
+    @commands.command(name = 'lock',
+                        command_prefix='u!',
+                        usage="<#channel/ID>",
+                        description="Lock the channel.")
+    @commands.guild_only()
+    async def lock (self, ctx, channel):
+
+        
+        channel = re.findall(r'\d+', channel) # 
+        channel = self.bot.get_channel(int(channel[0]))
+
+        if channel is None:
+            await channel.edit(name=f"Locked-{channel.name}")
+            await channel.set_permissions(ctx.guild.default_role, send_messages=False)
+            embed = discord.Embed(title = f"#{channel.name} channel(s) locked with success!", description = f"", color = 0x2fa737) 
+            await ctx.channel.send(embed = embed)
+        else:
+            await ctx.channel.send("Channel not found!")
+
+
+def setup(bot):
+    bot.add_cog(LockCog(bot))
