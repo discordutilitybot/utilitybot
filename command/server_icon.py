2161f5dc10c085bc2b53ef9aa378849bea37c1fa
andrew <andrewnijmeh1@gmail.com>
2020-12-11 22:18:36 -0500

    finish up server icon + add author embed color

--- command/server_icon.py
+++ command/server_icon.py
@@ -4,7 +4,17 @@ from discord.ext import commands
 class Servericon(commands.Cog):
     def __init__(self, bot) :
         self.bot = bot
-    
+
+    @commands.command()
+    async def server_icon(self, ctx):
+        member = ctx.author.color
+        guild = ctx.message.author
+        
+        await ctx.send(embed=discord.Embed(
+               color=member.color if member else ctx.author.color.set_image(
+                   url=str(guild.avatar_url_as(static_format='png', size=2048))
+               )
+            ))
 def setup(bot):
     bot.add_cog(Servericon(bot))
     bot.logging.info("Loaded Servericon command")
