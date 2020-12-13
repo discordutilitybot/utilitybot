ad567d49c2dcc7859c7e194655d9384c935984a3
andrew <andrewnijmeh1@gmail.com>
2020-12-13 17:13:31 -0500

    add setup tools

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from setuptools import setup
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(name='utilitybot',
+    author='Andrew Nijmeh',
+    url="https://discordutilitybot/utilitybot",
+    version='development',
+    packages=['command, cog, events, plugins, tests, utils, assets'],
+    license='MIT',
+    description='A open-source mutli-purpose discord bot with integration, music, moderation and alot more!',
+    long_description=readme,
+    include_package_data=False,
+    install_requires='requirements.txt',
+     python_requires='>=3.5.2',
+
+)
