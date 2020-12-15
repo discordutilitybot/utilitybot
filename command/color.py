e48852e06b6f43fb3c07d7c8cb3c62f54e9246a3
andrew <andrewnijmeh1@gmail.com>
2020-12-15 11:38:06 -0500

    use cog format + fix

--- command/color.py
+++ command/color.py
@@ -1,9 +1,19 @@
-color_number = 8
+import discord
+from discord.ext import commands
 
-color_picked = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+class Color(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def random_color(self, ctx):
+        color_number = 8
+
+        color_picked = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
             for i in range(number_of_colors)]
 
-await ctx.send(embed=discord.Embed(
-            color=color_picked 
-            description = "Random color: " + color_picked
-)
+        await ctx.send(embed=discord.Embed(color=color_picked, description = "Random color: " + color_picked)
+
+def setup(bot):
+    bot.add_cog(Color(bot))
+    bot.logging.info('Loaded color command')
